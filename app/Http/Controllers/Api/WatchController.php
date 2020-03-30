30421f2bb5a1ca919e467cdc49a622ee19cba54e
Laravel Shift <shift@laravelshift.com>
2020-03-30 13:45:19 +0000

    Leverage injected request object
    Laravel automatically injects the current Http [request object][1] to all Controller actions and Middleware. Leveraging this object improves consistency and testability.
    
    [1]: https://laravel.com/docs/requests#accessing-the-request

--- app/Http/Controllers/Api/WatchController.php
+++ app/Http/Controllers/Api/WatchController.php
@@ -1,22 +1,6 @@
-<?php
-
-namespace App\Http\Controllers\Api;
-
-use App\Http\Controllers\Controller;
-use App\Http\Requests\Api\WatchStoreRequest;
-use App\Watch;
-use Illuminate\Support\Facades\Response;
-
-class WatchController extends Controller
-{
-    /**
-     * @param \App\Http\Requests\Api\WatchStoreRequest $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(WatchStoreRequest $request)
     {
-        $watch = Watch::create(\Request::all());
+        $watch = Watch::create($request->all());
 
         return response(null, 204);
     }
-}
