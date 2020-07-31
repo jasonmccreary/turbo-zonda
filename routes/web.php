951805dd6bf7234b60f05ddadd9798c4b4f5c904
Jason McCreary <jason@pureconcepts.net>
2020-07-31 12:32:44 -0400

    More options array routes

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,28 @@ Route::group(['middleware' => 'auth'], function () {
     Route::resource('comment', 'CommentController', ['only' => ['create', 'show']]);
 });
 
+
+Route::get('dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@index']);
+
+
+Route::group(['middleware' => ['auth']], function () {
+  Route::get('billing', [
+      'as' => 'billing.create',
+      'uses' => 'BillingController@create',
+  ]);
+  Route::post('billing', [
+      'as' => 'billing.store',
+      'uses' => 'BillingController@store',
+  ]);
+
+  Route::get('invoices', [
+      'as'   => 'invoices.index',
+      'uses' => 'InvoiceController@index',
+  ]);
+  Route::get('invoices/{invoiceId}', [
+      'as'   => 'invoices.show',
+      'uses' => 'InvoiceController@show',
+  ]);
+});
+
 Route::resource('watch', 'WatchController')->only('store');
