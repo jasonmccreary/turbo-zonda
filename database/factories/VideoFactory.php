0c895a96a6047592990adfeb6fa0f50f1d827304
Laravel Shift <shift@laravelshift.com>
2020-09-11 19:20:52 +0000

    Shift to class based factories

--- database/factories/VideoFactory.php
+++ database/factories/VideoFactory.php
@@ -1,14 +1,32 @@
 <?php
 
-/** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Video;
-use Faker\Generator as Faker;
 
-$factory->define(Video::class, function (Faker $faker) {
-    return [
-        'title' => $faker->sentence(4),
-        'synopsis' => $faker->text,
-        'duration' => $faker->randomFloat(2, 0, 999999.99),
+class VideoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Video::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+        'title' => $this->faker->sentence(4),
+        'synopsis' => $this->faker->text,
+        'duration' => $this->faker->randomFloat(2, 0, 999999.99),
     ];
-});
+    }
+}
