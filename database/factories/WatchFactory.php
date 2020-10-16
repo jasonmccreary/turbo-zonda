e34b2115e96d6cab7be9501d3f3979416ce3da8d
Laravel Shift <shift@laravelshift.com>
2020-10-16 13:20:06 +0000

    Shift cleanup

--- database/factories/WatchFactory.php
+++ database/factories/WatchFactory.php
@@ -1,11 +1,9 @@
 <?php
 
-
-
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Watch;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class WatchFactory extends Factory
 {
